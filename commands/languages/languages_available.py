3980bdb9ed115a2c6af8580fdca2e7304b57b86a
Alexandre Pierre <alexandrempierre@gmail.com>
2021-11-22 17:21:03 -0300

    Fixed languages_available module

--- commands/languages/languages_available.py
+++ commands/languages/languages_available.py
@@ -4,7 +4,7 @@ Module containing the code corresponding to languages-available subcommand.
 '''
 
 
-__all__ = ['languages_available', 'language_name']
+__all__ = ['languages_available', 'language_name', 'save']
 __author__ = 'Alexandre Pierre'
 
 
@@ -12,27 +12,27 @@ import json
 
 
 LANGUAGES = {
-        'PT': 'Português 🇧🇷',
-        'ES': 'Espanhol 🇪🇸',
-        'EN': 'Inglês 🇺🇸',
-        'FR': 'Francês 🇫🇷'}
+        'pt': 'Português 🇧🇷',
+        'es': 'Espanhol 🇪🇸',
+        'en': 'Inglês 🇺🇸',
+        'fr': 'Francês 🇫🇷'}
 
 def language_name(abbr, abbr2name=None):
-    ''' '''
+    '''Given a language abbreviation returns the language name and a flag.'''
     if abbr2name is None:
         abbr2name = LANGUAGES
 
-    return abbr2name[abbr]
+    return abbr2name[abbr.lower()]
 
 
 def languages_available(soup):
-    ''' '''
+    '''Returns the languages in which the page is available.'''
     return map(
         lambda li: li.getText().strip(),
         soup.find('ul', {'class': 'nav idioma'}).find_all(
             'li', {'class': 'submenu'}))
 
-def save(filename, *values):
-    ''' '''
+def save(filename, *languages):
+    '''Save the languages passed as arguments.'''
     with open(filename, 'w') as file_ptr:
-        json.dump([{'lingua': value.lower()} for value in values], file_ptr)
+        json.dump([{'lingua': lang.lower()} for lang in languages], file_ptr)
